502fa5d75a6868ecf082176ea55cffc2038d2bd6
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-06-05 22:39:47 +0530

    Project completed added toggle function

--- 04-accordion/final/src/Question.js
+++ 04-accordion/final/src/Question.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title,info}) => {
+  const [isExpanded,setExpanded] = useState(false);
+  return (
+    <section className="question">
+      <header>  
+        <h4>{title}</h4>
+        <button className="btn" onClick = {() => setExpanded(!isExpanded)}>
+          {isExpanded?<AiOutlineMinus/>:<AiOutlinePlus/>}
+        </button>
+      </header>
+      {isExpanded && <p>{info}</p>}
+    </section>
+  );
 };
 
 export default Question;
